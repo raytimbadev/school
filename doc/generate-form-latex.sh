268edb2ef0aed11ca9a2d7b675c473c66ae10403
Jacob Errington <jacob.errington@mail.mcgill.ca>
2015-03-18 16:53:57 -0400

    adjust comments in script for generating the HGEN 396 application form

--- doc/generate-form-latex.sh
+++ doc/generate-form-latex.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Match the part of the line up to the colon. That will go between the curly
-# braces in \q{}. The remainder of the line, if any, will go after, as a
-# pre-supplied value for that field.
+# Match the part of the line up to the colon as the item to describe, and the
+# part after that as the description.
 echo '\begin{description}'
 cat form-data.txt | perl -pe 's|^(.*?):(.*?)$|\\item\[\1\]\2|'
 echo '\end{description}'
