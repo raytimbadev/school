4cc805c1e8ae0111c4b964841b2772b68cb96294
ash <alaporte.live@gmail.com>
2015-10-08 15:09:01 -0400

    Fixed flights

--- src/middleware/MiddlewareManagerImpl.java
+++ src/middleware/MiddlewareManagerImpl.java
@@ -324,8 +324,8 @@ public class MiddlewareManagerImpl implements server.ws.ResourceManager {
 		}
 		StringBuilder sb = new StringBuilder();
 		String s1 = flightManager.queryCustomerInfo(id, customerId); 
-		String s2 = flightManager.queryCustomerInfo(id, customerId); 
-		String s3 = flightManager.queryCustomerInfo(id, customerId); 
+		String s2 = roomManager.queryCustomerInfo(id, customerId); 
+		String s3 = carManager.queryCustomerInfo(id, customerId); 
 		sb.append(s1); 
 		sb.append(s2); 
 		sb.append(s3); 
