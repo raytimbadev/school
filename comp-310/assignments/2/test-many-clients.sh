f77ff8a54eb820bb742fa490e1ad1e6b658b054a
Jacob Errington <git@mail.jerrington.me>
2015-10-19 12:06:55 -0400

    comp 310 assignment 2: refactor testing script to use a loop

--- comp-310/assignments/2/test-many-clients.sh
+++ comp-310/assignments/2/test-many-clients.sh
@@ -2,22 +2,45 @@
 
 CLIENT="client/bin/client"
 
-$CLIENT 1 "Captain: What happen ? "
-$CLIENT 1 "Mechanic: Somebody set up us the bomb. "
-$CLIENT 1 "Operator: We get signal. "
-$CLIENT 1 "Captain: What !"
-$CLIENT 1 "Operator: Main screen turn on. "
-$CLIENT 1 "Captain: It’s you !! "
-$CLIENT 1 "CATS: How are you gentlemen !!"
-$CLIENT 1 "CATS: All your base are belong to us. "
-$CLIENT 1 "CATS: You are on the way to destruction. "
-$CLIENT 1 "Captain: What you say !!"
-$CLIENT 1 "CATS: You have no chance to survive make your time."
-$CLIENT 1 "CATS: Ha ha ha ha … "
-$CLIENT 1 "Operator: Captain !!"
-$CLIENT 1 "Captain: Take off every ‘ZIG’!! "
-$CLIENT 1 "Captain: You know what you doing."
-$CLIENT 1 "Captain: Move ‘ZIG’."
-$CLIENT 1 "Captain: For great justice."
+test \! -x "$CLIENT" && eval "echo 'run make first' ; exit 1"
+
+while read line && read duration ; do
+    $CLIENT $duration "$line"
+done <<EOF
+Captain: What happen ?
+1
+Mechanic: Somebody set up us the bomb.
+2
+Operator: We get signal.
+1
+Captain: What !
+1
+Operator: Main screen turn on.
+2
+Captain: It’s you !!
+1
+CATS: How are you gentlemen !!
+2
+CATS: All your base are belong to us.
+2
+CATS: You are on the way to destruction.
+2
+Captain: What you say !!
+1
+CATS: You have no chance to survive make your time.
+2
+CATS: Ha ha ha ha ...
+4
+Operator: Captain !!
+1
+Captain: Take off every ‘ZIG’!!
+1
+Captain: You know what you doing.
+2
+Captain: Move ‘ZIG’.
+1
+Captain: For great justice."
+1
+EOF
 
 wait
