d692ee95bc6b8f72b492e27aa2889130513a8fcf
Jacob Errington <git@mail.jerrington.me>
2015-11-12 14:35:23 -0500

    improve concurrency testing script

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 function badcmdargs () {
-    echo "usage: $0 <number of concurrent clients> <loop count> <transactions per second>" >&2
+    echo "usage: $0 <number of concurrent clients> <loop count> <transactions per second> <mode>" >&2
     exit 1
 }
 
@@ -10,8 +10,9 @@ LOGDIR=logs
 PROC_COUNT=$1
 LOOPCOUNT=$2
 TPS=$3
+MODE=$4
 
-test $# -ne 3 && badcmdargs
+test $# -ne 4 && badcmdargs
 
 mkdir -p "${LOGDIR}-${PROC_COUNT}"
 
@@ -20,7 +21,9 @@ do
     ant "-Dtest-filename=${LOGDIR}-${PROC_COUNT}/${i}.txt" \
         "-Dtest-tps=$TPS" \
         "-Dtest-loop-count=$LOOPCOUNT" \
-        test-client &
+        "-Dtest-setting=$MODE" \
+        test-client > "${LOGDIR}-${PROC_COUNT}/${i}-out.txt" &
 done
 
+echo "Running $PROC_COUNT concurrent clients."
 wait
