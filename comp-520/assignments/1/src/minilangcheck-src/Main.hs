45344553a916c6b0d88e6a100227060a670f746f
Jacob Errington <git@mail.jerrington.me>
2016-01-18 12:38:51 -0500

    comp 520 assignment 1: restructure to conform to required format
    All the code is moved into `src`, as required.
    The cabal build script now compiles a `minilancheck` executable taking
    one file name as a command-line argument, printing "Valid" if the
    file parses and pringing "Invalid" otherwise.
    
    Building and running instructions are written in README.
    A name is proposed for the dragon in DragonName.txt.
    A script `run` is added to first try to run a locally built
    `minilangcheck` and fall back to a predistributed `minilangcheck` placed
    in `src`.

---
+++ comp-520/assignments/1/src/minilangcheck-src/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Language.Minilang
+
+import Data.Text.IO ( readFile )
+import Prelude hiding ( readFile )
+import Options.Applicative
+
+argParser = info sourcePath details where
+    sourcePath = argument str (metavar "FILE")
+    details = header h <> progDesc p
+    h = "minilangcheck - checks the syntax of a minilang program"
+    p = "The specified FILE is loaded and parsed. "
+        ++ "If the parse succeeds, then `Valid` is printed to standard out; " 
+        ++ "else, `Invalid` is printed."
+
+main :: IO ()
+main = do
+    sourcePath <- execParser argParser 
+    contents <- readFile sourcePath
+    case parseOnlyMinilang sourcePath contents of
+        Left e -> do
+            putStrLn "Invalid"
+            print e
+        Right _ -> do
+            putStrLn "Valid"
