3a0c57faadabcc354d955a620f97af5c79192334
Jacob Errington <git@mail.jerrington.me>
2016-02-01 10:26:36 -0500

    comp 520 minilang: improve compiler frontend
    The compiler frontend conforms to the assignment requirements:
    
    * the computed symbol table is written out to "X.symbol.txt"
    * the pretty-printed minilang code is written out to "X.pretty.min"
    * the generated C code is written out to "X.c"
    
    where X is the the input file name minus the ".min" extension.
    
    If a parse error occurs, it is reported and compilation aborts.
    If a semantic error, it is reported and compilation aborts.

--- comp-520/minilang/src/minilangc/Main.hs
+++ comp-520/minilang/src/minilangc/Main.hs
@@ -5,17 +5,15 @@ module Main where
 import Language.Minilang
 import Language.Minilang.Codegen
 import Language.Minilang.Typecheck
-import Language.Minilang.SrcAnn
 
 import Control.Monad.Except
 import Data.Functor.Foldable
 import Data.Text ( pack )
-import Data.Text.IO ( readFile, getContents )
--- import qualified Data.Text.IO as TIO
+import Data.Text.IO ( readFile, writeFile, getContents )
 import qualified Language.C.Pretty as CP
 import Options.Applicative
-import Prelude hiding ( readFile, getContents )
--- import System.Exit ( exitFailure )
+import Prelude hiding ( readFile, writeFile, getContents )
+import System.FilePath
 import Text.PrettyPrint hiding ( (<>) )
 
 unFix :: Fix f -> f (Fix f)
@@ -25,41 +23,47 @@ argParser :: ParserInfo String
 argParser = info sourcePath details where
     sourcePath = argument str (metavar "FILE")
     details = header h <> progDesc p
-    h = "minilangcheck - checks the syntax of a minilang program"
+    h = "minilangc - checks the syntax of a minilang program"
     p = "The specified FILE is loaded and parsed. "
-        ++ "If the parse succeeds, then `Valid` is printed to standard out; " 
+        ++ "If the parse succeeds, then `Valid` is printed to standard out; "
         ++ "else, `Invalid` is printed. "
         ++ "The special file - can be used to read from standard in."
 
 main :: IO ()
 main = do
     sourcePath <- execParser argParser
-    contents <- if sourcePath == "-" then getContents else readFile sourcePath
+    (sourcePath', contents) <- if sourcePath == "-"
+        then (,) <$> pure "stdin"    <*> getContents
+        else (,) <$> pure sourcePath <*> readFile sourcePath
 
-    case parseOnlyMinilang (case sourcePath of "-" -> "stdin" ; x -> x) contents of
+    case parseOnlyMinilang sourcePath' contents of
         Left e -> do
-            putStrLn "Invalid"
+            putStrLn "Parse error."
             print e
-        Right p -> do
-            putStrLn "Valid\n"
-            putStrLn "Pretty print:"
-            let roundT = render (pretty p)
-            putStrLn roundT
+        Right p -> handleParsed sourcePath' p
 
-            case parseOnlyMinilang "roundtrip" (pack roundT) of
-                Left e -> do
-                    putStrLn "roundtrip failed"
-                    print e
-                Right e -> do
-                    putStrLn "roundtrip succeeded"
-                    putStrLn (render (pretty e))
+handleParsed :: FilePath -> SrcAnnProgram -> IO ()
+handleParsed sourcePath p = do
+    -- write the pretty-printed minilang code
+    let pp = pack . render . pretty . unannotateProgram $ p
+    writeFile (sourcePath -<.> "pretty.min") pp
 
-            case runExcept (typecheckProgram p) of
-                Left (Ann pos e') -> do
-                    putStrLn "type error:"
-                    print e'
-                    putStrLn "at:"
-                    print (srcStart pos)
-                Right p' -> do
-                    let c = translateProgramMain p'
-                    putStrLn (render (CP.pretty c))
+    -- compute the symbol table from the declarations list
+    let (Program decls _) = p
+    let symtab = symbolTableFrom (map unannotateDecl decls)
+
+    -- write out the symbol table in a tab-separated format
+    writeFile (sourcePath -<.> "symbol.txt") (pack (symbolTableTsv symtab))
+
+    -- typecheck the program
+    case runExcept (typecheckProgram p) of
+        Left e -> putStrLn (render (pretty e))
+        Right p' -> handleTypechecked sourcePath p'
+
+handleTypechecked :: FilePath -> TySrcAnnProgram -> IO ()
+handleTypechecked sourcePath p = do
+    -- perform code generation
+    let c = translateProgramMain p
+
+    -- write out the generated code
+    writeFile (sourcePath -<.> "c") (pack (render (CP.pretty c)))
