c4e0a27399c412f31360337ade71e9c6a4f60347
Jacob Errington <git@mail.jerrington.me>
2016-02-02 21:16:49 -0500

    comp 520 minilang: housekeeping
    * Made symlinks to the semantically invalid programs.
    * Adjusted the package script to copy minilangc
    * Adjusted the run script to use minilangc

--- comp-520/minilang/package.sh
+++ comp-520/minilang/package.sh
@@ -6,8 +6,8 @@ cd src
 cabal build
 cd ../..
 rm -rf jacob_errington
-cp -rf 1 jacob_errington
-mv -v jacob_errington/src/{dist/build/minilangcheck/minilangcheck,}
+cp -rf minilang jacob_errington
+mv -v jacob_errington/{src/dist/build/minilangc/minilangc,}
 rm -rf jacob_errington/src/{.cabal-sandbox,cabal.sandbox.config}
 rm -rf jacob_errington/src/dist
 tar zcvf jacob_errington.tar.gz jacob_errington/
